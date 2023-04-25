c50c40c3e89aa46d6758667923f6d000c06c29ae
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-25 10:29:35 -0300

    feat: Add função Divisão

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@
         Console.WriteLine($"{valor1} + {valor2} = {resultado}");
     }
 
-        static void Subtracao()
+    static void Subtracao()
     {
         Console.Write("Primeiro valor: ");
         float valor1 = float.Parse(Console.ReadLine());
@@ -33,6 +33,19 @@
 
         Console.WriteLine($"{valor1} + {valor2} = {resultado}");
 
+    }
+
+    static void Divisao()
+    {
+        Console.Write("Primeiro valor: ");
+        float valor1 = float.Parse(Console.ReadLine());
+
+        Console.Write("Segundo valor: ");
+        float valor2 = float.Parse(Console.ReadLine());
+
+        float resultado = valor1 / valor2;
+
+        Console.WriteLine($"{valor1} / {valor2} = {resultado}");
     }
 
 }
