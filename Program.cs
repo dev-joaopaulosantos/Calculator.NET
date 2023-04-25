1c6c7339243081ba91e528cec723b70aeaac3e50
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-24 21:56:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Primeiro valor: ");
+        float valor1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine(valor1);
+    }
+}
