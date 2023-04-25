4d9d0e8041dcdd73dfd02402ccdc636094f8ec71
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-25 10:27:51 -0300

    feat: Add função Soma

--- Program.cs
+++ Program.cs
@@ -3,9 +3,21 @@
     private static void Main(string[] args)
     {
         Console.Clear();
+        Soma();
+
+        Console.ReadKey();
+    }
+
+    static void Soma()
+    {
         Console.Write("Primeiro valor: ");
         float valor1 = float.Parse(Console.ReadLine());
 
-        Console.WriteLine(valor1);
+        Console.Write("Segundo valor: ");
+        float valor2 = float.Parse(Console.ReadLine());
+
+        float resultado = valor1 + valor2;
+
+        Console.WriteLine($"{valor1} + {valor2} = {resultado}");
     }
 }
