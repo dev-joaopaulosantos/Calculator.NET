6d5c1b572f2d606def4c874b5e2825d4d1ece355
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-25 10:28:55 -0300

    feat: Add função Subtração

--- Program.cs
+++ Program.cs
@@ -20,4 +20,19 @@
 
         Console.WriteLine($"{valor1} + {valor2} = {resultado}");
     }
+
+        static void Subtracao()
+    {
+        Console.Write("Primeiro valor: ");
+        float valor1 = float.Parse(Console.ReadLine());
+
+        Console.Write("Segundo valor: ");
+        float valor2 = float.Parse(Console.ReadLine());
+
+        float resultado = valor1 - valor2;
+
+        Console.WriteLine($"{valor1} + {valor2} = {resultado}");
+
+    }
+
 }
